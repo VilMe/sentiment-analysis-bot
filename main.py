b02a9d401473a392401c1fb8d9d9da2264e1dc1d
VilMe <vilma.medrano00@gmail.com>
2025-10-17 23:11:22 -0500

    define get_mood function

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ from dataclasses import dataclass
 class Mood:
     emoji: str
     sentiment: float
+
+
+def get_mood(input_text: str, *, sensitivity: float) -> Mood:
+    polarity: float = TextBlob(input_text).sentiment.poloarity
+
+    friendly_threshold: float = sensitivity
+    hostile_threshold: float = -sensitivity
+
+    if polarity >= friendly_threshold:
+        return Mood('😊', polarity)
+    elif polarity <= hostile_threshold:
+        return Mood('😠', polarity)
+    else: 
+        return Mood('😐', polarity)
