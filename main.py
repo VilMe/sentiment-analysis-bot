dffe5f4017954e2a49005eb0b267e48ecc485544
VilMe <vilma.medrano00@gmail.com>
2025-10-17 23:41:06 -0500

    add if name to run run_bot function and correct typos that caused bugs

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Mood:
 
 
 def get_mood(input_text: str, *, sensitivity: float) -> Mood:
-    polarity: float = TextBlob(input_text).sentiment.poloarity
+    polarity: float = TextBlob(input_text).sentiment.polarity
 
     friendly_threshold: float = sensitivity
     hostile_threshold: float = -sensitivity
@@ -24,11 +24,13 @@ def get_mood(input_text: str, *, sensitivity: float) -> Mood:
     
 
 def run_bot():
-    print('Enter ssome text to get a sentiment analysis back: ')
+    print('Enter some text to get a sentiment analysis back: ')
     while True:
         user_input: str = input ('You: ')
         mood: Mood = get_mood(user_input, sensitivity=0.3)
         print(f'Bot: {mood.emoji} ({mood.sentiment})')
 
 
-        
+
+if __name__ == '__main__':
+    run_bot()
